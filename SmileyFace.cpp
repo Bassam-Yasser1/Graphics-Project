dd5431d6f72f6e1ce32968772b56855295c44025
Mariam Ayman <maryoussef149@gmail.com>
2026-05-06 14:26:14 +0300

    Update SmileyFace.cpp

--- SmileyFace.cpp
+++ SmileyFace.cpp
@@ -2,18 +2,25 @@
 #include "Circles.h"
 
 void DrawSmiley(HDC hdc, int xc, int yc, bool isHappy) {
-    // Head and Eyes
-    CircleMidpoint(hdc, xc, yc, 60, RGB(0, 0, 0));
-    CircleMidpoint(hdc, xc - 20, yc - 20, 7, RGB(0, 0, 0));
-    CircleMidpoint(hdc, xc + 20, yc - 20, 7, RGB(0, 0, 0));
+    // 1. Draw Face Outline & Eyes
+    CircleMidpoint(hdc, xc, yc, 60, RGB(0, 0, 0));       // Face
+    CircleMidpoint(hdc, xc - 20, yc - 20, 7, RGB(0, 0, 0)); // Left Eye
+    CircleMidpoint(hdc, xc + 20, yc - 20, 7, RGB(0, 0, 0)); // Right Eye
     
-    // Nose
-    MoveToEx(hdc, xc, yc - 5, NULL); LineTo(hdc, xc, yc + 15);
+    // 2. Draw Nose
+    MoveToEx(hdc, xc, yc - 5, NULL); 
+    LineTo(hdc, xc, yc + 15);
     
-    // Mouth
+    // 3. Draw Mouth (The Arc)
     if (isHappy) {
-        Arc(hdc, xc - 30, yc + 5, xc + 30, yc + 35, xc + 30, yc + 20, xc - 30, yc + 20);
+        // A smile uses the BOTTOM half of the ellipse
+        // Start point is left, end point is right
+        Arc(hdc, xc - 30, yc + 10, xc + 30, yc + 40, 
+                 xc - 30, yc + 25, xc + 30, yc + 25);
     } else {
-        Arc(hdc, xc - 30, yc + 15, xc + 30, yc + 45, xc - 30, yc + 30, xc + 30, yc + 30);
+        // A frown uses the TOP half of the ellipse
+        // Start point is right, end point is left (reverses direction)
+        Arc(hdc, xc - 30, yc + 20, xc + 30, yc + 50, 
+                 xc + 30, yc + 35, xc - 30, yc + 35);
     }
 }
