ac8c6bb0c011a55b7cc47ffbf0f5aba6ea78a779
Mariam Ayman <maryoussef149@gmail.com>
2026-05-12 19:29:28 +0300

    Update Preferences.h

--- Preferences.h
+++ Preferences.h
@@ -1,22 +1,8 @@
 #pragma once
 #include <windows.h>
 
-// -------------------------------------------------------
-// Preferences.h
-// User-preference helpers:
-//   a) Set window background to white
-//   b) Change the window cursor shape
-//   c) Let the user pick a drawing colour via ColorDlg
-// -------------------------------------------------------
-
-// a) Repaints the client area with a solid white background.
-//    Call after WM_COMMAND for IDM_PREF_BG_WHITE.
 void PrefSetWhiteBackground(HWND hwnd);
 
-// b) Changes the class cursor to one of the built-in IDC_ shapes.
-//    cursorId: one of IDC_ARROW, IDC_CROSS, IDC_IBEAM, IDC_HAND, etc.
 void PrefSetCursor(HWND hwnd, LPCWSTR cursorId);
 
-// c) Opens the system Color-picker dialog.
-//    Returns the chosen COLORREF, or the current colour if cancelled.
 COLORREF PrefChooseColor(HWND hwnd, COLORREF currentColor);
