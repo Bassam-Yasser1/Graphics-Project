17c519ce0ceb188c8bad1c4c884a97b284543e08
Mariam Ayman <maryoussef149@gmail.com>
2026-05-12 19:29:57 +0300

    Update SmileyFace.cpp

--- SmileyFace.cpp
+++ SmileyFace.cpp
@@ -11,14 +11,12 @@ void DrawSmiley(HDC hdc, int xc, int yc, bool isHappy) {
     MoveToEx(hdc, xc, yc - 5, NULL); 
     LineTo(hdc, xc, yc + 15);
     
-    // 3. Draw Mouth (The Arc)
+    // 3. Draw Mouth (Arc)
     if (isHappy) {
-        // A smile uses the BOTTOM half of the ellipse
         // Start point is left, end point is right
         Arc(hdc, xc - 30, yc + 10, xc + 30, yc + 40, 
                  xc - 30, yc + 25, xc + 30, yc + 25);
     } else {
-        // A frown uses the TOP half of the ellipse
         // Start point is right, end point is left (reverses direction)
         Arc(hdc, xc - 30, yc + 20, xc + 30, yc + 50, 
                  xc + 30, yc + 35, xc - 30, yc + 35);
