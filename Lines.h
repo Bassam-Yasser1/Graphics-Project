4400e2668b6f19164905776eb6668187ed972b6e
Mariam Ayman <maryoussef149@gmail.com>
2026-05-12 19:28:45 +0300

    Update Lines.h

--- Lines.h
+++ Lines.h
@@ -1,15 +1,6 @@
 #pragma once
 #include <windows.h>
 
-// -------------------------------------------------------
-// Lines.h
-// Line drawing algorithms:
-//   - DDA (Digital Differential Analyzer)
-//   - Midpoint Line
-//   - Parametric Line
-// Each takes two endpoints and a colour.
-// -------------------------------------------------------
-
 void DrawLineDDA        (HDC hdc, int x1, int y1, int x2, int y2, COLORREF color);
 void DrawLineMidpoint   (HDC hdc, int x1, int y1, int x2, int y2, COLORREF color);
 void DrawLineParametric (HDC hdc, int x1, int y1, int x2, int y2, COLORREF color);
